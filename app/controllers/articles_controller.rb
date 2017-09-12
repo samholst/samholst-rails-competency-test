386aa6707d43d261b066c53cba7cec334d613d4e
Sam Holst <sawohol@gmail.com>
2017-09-12 16:32:38 -0600

    adding in more permissions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  access all: [:index], editor_user: :all, admin_user: {except: [:destroy, :new, :create, :update, :edit]}
+  access all: [:index], user: {except: [:destroy, :new, :create, :update, :edit]}, editor_user: :all, admin_user: {except: [:destroy, :new, :create, :update, :edit]}
 
   def index
     @articles = Article.all
