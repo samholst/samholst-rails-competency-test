d8790e28087a17d3413b367063c10bb859d6dda3
Sam Holst <sawohol@gmail.com>
2017-09-12 14:23:07 -0600

    finished touches on back-end

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def index
-    @car_articles = Article.where(category: "Cars")
-    @ruby_articles = Article.where(category: "Ruby")
-    @game_articles = Article.where(category: "Games")
+    @car_articles = Article.where(category: "Cars").limit(3)
+    @ruby_articles = Article.where(category: "Ruby").limit(3)
+    @game_articles = Article.where(category: "Games").limit(3)
   end
 end
