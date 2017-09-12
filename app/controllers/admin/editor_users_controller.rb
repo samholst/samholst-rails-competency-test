7a31b5c8e468c74c42bbf04c5a84d0219e8b34e3
Sam Holst <sawohol@gmail.com>
2017-09-12 17:22:17 -0600

    addd administrate gem

---
+++ app/controllers/admin/editor_users_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  class EditorUsersController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # you can overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = EditorUser.
+    #     page(params[:page]).
+    #     per(10)
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   EditorUser.find_by!(slug: param)
+    # end
+
+    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :articles
+    resources :admin_users
+    resources :editor_users
+
+    root to: "users#index"
+  end
+
   resources :articles
   devise_for :users
   get 'static/index'
