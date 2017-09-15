d33691489dd6a944e82a69e05f3bd4bdd9e363f5
Sam Holst <sawohol@gmail.com>
2017-09-14 20:48:28 -0600

    refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :check_owner, only: [:update, :edit, :destroy]
-  access all: [:index], user: {except: [:destroy, :new, :create, :update, :edit]}, editor_user: :all, admin_user: {except: [:destroy, :new, :create, :update, :edit]}
+  access all: [:index],
+         user: {except: [:destroy, :new, :create, :update, :edit]},
+         editor_user: :all, admin_user: {except: [:destroy, :new, :create, :update, :edit]}
 
   def index
     @articles = Article.all
