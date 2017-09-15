582573a026418a4ff163a593abbfbd7554f396c9
Sam Holst <sawohol@gmail.com>
2017-09-15 09:22:32 -0600

    added notifications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require materialize/extras/nouislider
+//= require gritter
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def crud_access
     ["AdminUser", "EditorUser"]
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:notice] || flash[:error])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Alert!", sticky: false)
+  end
 end
