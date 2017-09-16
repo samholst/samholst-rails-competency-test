068c35469ebf279a0b281b58c7068cf314dfd9c9
Sam Holst <sawohol@gmail.com>
2017-09-15 19:53:11 -0600

    refactored for first name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
