48c783d18d2c289bb31e2ee8329cc6d8958de17b
Sam Holst <sawohol@gmail.com>
2017-09-15 20:09:40 -0600

    fixed bug that allowed admins to use CRUD for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-    resources :articles
+    # resources :articles
     resources :admin_users
     resources :editor_users
 
