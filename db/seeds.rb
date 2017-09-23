d698892b1adb3bc61c607211958f75063ab213f4
Sam Holst <sawohol@gmail.com>
2017-09-23 16:24:54 -0600

    correct spelling

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,7 +61,7 @@ end
 puts "3 editor user ruby articles created"
 
 3.times do |hackers|
-  Article.create!(title: "Ruby My test #{hackers}",
+  Article.create!(title: "Hackers My test #{hackers}",
                   content: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish.",
                   category: "Hackers",
                   user_id: @editor_user_2.id
